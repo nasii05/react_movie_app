aa914e4888fab9f2b2117e617c2ab6769ec04f41
nasii05 <iamnasih@gmail.com>
2025-06-10 20:08:46 +0530

    show movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Search from "./components/Search.jsx";
 import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner.jsx";
 
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -20,6 +21,9 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+      setIsLoading(true);
+      setErrorMessage('');
+
       try{
           const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
@@ -33,10 +37,16 @@ const App = () => {
 
           if(data.response === 'False'){
               setErrorMessage(data.Error || 'Failed to fetch movies');
+              setMovieList([]);
+              return;
           }
+
+          setMovieList(data.results || []);
       }catch(error){
           console.log(`Error fetching movies: ${error}`);
           setErrorMessage('Error fetching movies. Please try again later');
+      } finally {
+          setIsLoading(false);
       }
   }
 
@@ -59,9 +69,19 @@ const App = () => {
             </header>
 
             <section className="all-movies">
-                <h2>All movies</h2>
+                <h2 className="mt-[40px]">All movies</h2>
 
-                {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+                {isLoading ? (
+                    <Spinner/>
+                ) : errorMessage ? (
+                   <p className="text-red-500">{errorMessage}</p>
+                ) : (
+                    <ul>
+                        {movieList.map((movie) => (
+                            <p key={movie.id} className="text-white">{movie.title}</p>
+                        ))}
+                    </ul>
+                )}
             </section>
         </div>
     </main>
