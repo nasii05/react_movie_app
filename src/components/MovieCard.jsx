72663acd429f6fdfcfd22b94528627b08f039ea2
nasii05 <iamnasih@gmail.com>
2025-06-11 10:38:14 +0530

    movie search

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie: { title, vote_average, poster_path, original_language, release_date } }) => {
     return (
-        <p key={movie.id} className="text-white">{movie.title}</p>
+        <div className="movie-card">
+          <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` :
+          '/no-movie.png'} alt="movie-image" />
+
+          <div className="mt-4">
+              <h3>{title}</h3>
+
+              <div className="content">
+                  <div className="rating">
+                      <img src="star.svg" alt="star Icon" />
+                      <p>{vote_average ? vote_average.toFixed(1) : "N/A"} </p>
+                  </div>
+
+                  <span>•</span>
+                  <p className="lang">{original_language}</p>
+                  <span>•</span>
+                  <p className="year">{release_date ? release_date.split("_")[0] : 'N/A'}</p>
+              </div>
+          </div>
+        </div>
     )
 }
 export default MovieCard;
